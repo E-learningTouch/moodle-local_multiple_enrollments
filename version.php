fa3ce16e3a09cb93403092302fcb41255c72f667
Luiggi Sansonetti <1565841+luiggisanso@users.noreply.github.com>
2026-04-13 18:50:51 +0200

    Update version.php

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
  defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_multiple_enrollments';
-$plugin->version = 2025060300;
+$plugin->version = 2026041300;
 $plugin->requires = 2022112800; // Moodle 4.1!
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '2.0.0';
